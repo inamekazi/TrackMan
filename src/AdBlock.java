8942761f3e08405b09e6d09c98a7b52db9c0bd5b
inamekazi <aaguilar@macalester.edu>
2020-10-17 17:55:44 -0500

    Show

--- src/AdBlock.java
+++ src/AdBlock.java
@@ -5,10 +5,7 @@ import java.awt.*;
 import java.security.MessageDigest;
 
 public class AdBlock extends Defenses {
-    private static final String MESSAGE = "You have obtained an Ad-Blocker!" +
-            "Ad blockers are a great way to avoid getting your data collected!" +
-            " They do so by blocking requests from your browser to domains that" +
-            " are known to belong to trackers! The blockers can also restrict the types of cookies that domains can set! Enjoy being advertisement tracker free!";
+    private static final String MESSAGE = "You have obtained an Ad-Blocker! Ad blockers are a great way to avoid getting your data collected! They do so by blocking requests from your browser to domains that are known to belong to trackers! The blockers can also restrict the types of cookies that domains can set! Enjoy being advertisement tracker free!";
 
 
     public AdBlock(CanvasWindow canvas, double x, double y ) {
